dfdf733fd6bc3f6f7fe915f924337f9e25ac79f4
Auriellle <44744455+Auriellle@users.noreply.github.com>
2019-04-06 20:37:34 +0200

    Prettified cps counter

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function addCookie(numb) {
     cookies += numb;
     //cookies = magnify(cookies);
     document.getElementById("cookieCount").innerHTML = prettify(cookies); 
-    document.getElementById("perSecond").innerHTML = 0.1*buildingOneCount + 2*buildingTwoCount;
+    document.getElementById("perSecond").innerHTML = prettify(0.1*buildingOneCount + 2*buildingTwoCount);
 }
 
 
