b1bae9bf5e94bdaee4ebbe8e7395179e65389f81
Auriellle <44744455+Auriellle@users.noreply.github.com>
2019-04-14 20:55:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ function addCookie(numb) {
     cookieCount = cookies;
     cookieCount = magnify(cookieCount);
     document.getElementById("cookieCount").innerHTML = cookieCount;
-    document.getElementById("perSecond").innerHTML = prettify(0.1 * allBuildings[0].getCount() + 1 * allBuildings[2].getCount());
+    document.getElementById("perSecond").innerHTML = prettify(0.1 * buildingOne.getCount() + 1 * buildingTwo.getCount());
 }
