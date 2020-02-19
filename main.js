b0603adc2bbbc9d27dda12d93320046228532334
alexisBes <32544682+alexisBes@users.noreply.github.com>
2020-02-19 13:47:24 +0100

    Update cultust every second wahn the page is active

--- main.js
+++ main.js
@@ -8,4 +8,12 @@ function addCookie(numb) {
     document.getElementById("perSecond").innerHTML = prettify(0.1*allBuildings[1].getCount() + 1*allBuildings[2].getCount());
 }
 
-
+function updateCookie() {
+    setInterval(function() {
+        var valPerSec  = Number(document.getElementById("perSecond").innerHTML);
+        if (!isNaN(valPerSec)) {
+            addCookie(valPerSec);
+        }
+    }, 1000)
+   
+}
